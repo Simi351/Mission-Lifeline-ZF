80e92d60313410d91ee5585d991ca42834c2a448
Simi351 <Wolf@horizonte-ggmbh.com>
2026-08-21 06:04:48 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAmVGjH6VstNu9iP2c0W27rTEKkIEegMfg",
+  authDomain: "lifeline-25064.firebaseapp.com",
+  databaseURL: "https://lifeline-25064-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "lifeline-25064",
+  storageBucket: "lifeline-25064.firebasestorage.app",
+  messagingSenderId: "40793878622",
+  appId: "1:40793878622:web:5e2843b491a829be18c4cf",
+  measurementId: "G-74F959206S"
+};
+
+export const gameMasterEmail = "wolf@horizonte-ggmbh.com";
